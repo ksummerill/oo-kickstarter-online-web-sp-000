3fbbe7fed0db642c271b68e8ecbe818bac053c8b
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-11 20:11:17 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,8 +10,8 @@ class Backer
   # takes in a Project instance and adds the project to its @backed_projects attribute.
   # also adds the backer to the project's backers array
   def back_project(name)
-    # @backed_projects << name
-    Project.add_backer << name
+    @backed_projects << name
+    # Project.add_backer << name
   end
 
 end
