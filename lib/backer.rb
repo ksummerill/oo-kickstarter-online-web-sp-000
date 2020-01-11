8a19bc3323bdcce9197c87847f80163b1d10a959
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-11 20:04:09 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,15 @@
 class Backer
 
-  attr_reader :backed_projects
+  attr_reader :backed_projects, :name
 
-  def initialize
+  def initialize(name)
+    @name = name
     @backed_projects = []
   end
 
+  # takes in a Project instance and adds the project to its @backed_projects attribute.
+  def back_project()
+    @backed_projects << 
+  end
 
 end
